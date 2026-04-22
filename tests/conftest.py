bd16f46486c8e7ae02677a0892cf910acb37a68f
Artem <artem.kotyakhov@gmail.com>
2026-04-22 13:01:42 +0300

    add fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+from typing import Generator
+
+import pytest
+from playwright.sync_api import sync_playwright, Page
+
+
+@pytest.fixture
+def chromium_page() -> Generator[Page]:
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        yield browser.new_page()
+        browser.close()
