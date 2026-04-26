8615c39f6164abf30eb4ff518ac8d718d5e017ec
Artem <artem.kotyakhov@gmail.com>
2026-04-26 18:18:30 +0300

    refactor: add '_alert' to method name

--- pages/login_page.py
+++ pages/login_page.py
@@ -32,6 +32,6 @@ class LoginPage(BasePage):
         self.registration_link.click()
 
     # Метод для проверки отображения алерта с ошибкой
-    def check_visible_wrong_email_or_password(self):
+    def check_visible_wrong_email_or_password_alert(self):
         expect(self.wrong_email_or_password_alert).to_be_visible()
         expect(self.wrong_email_or_password_alert).to_have_text('Wrong email or password')
